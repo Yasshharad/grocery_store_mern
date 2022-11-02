812bfc08c39b805db506f10dac799bbbf83ab06e
Priyankaabuj <it.a.01.priyanka.abuj@gmail.com>
2022-11-02 22:36:49 +0530

    added server side of category

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ app.use(fileUpload({
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
-// app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/categoryRouter'))
 // app.use('/api', require('./routes/upload'))
 // app.use('/api', require('./routes/productRouter'))
 // app.use('/api', require('./routes/paymentRouter'))
